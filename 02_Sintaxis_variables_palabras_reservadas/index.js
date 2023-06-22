2df57f50957865c4ad5e89f1030050a2d8ccb760
Nuria Campo <nuria.campo7@gmail.com>
2023-06-22 12:20:18 +0000

    Ejercicio tema 2

---
+++ 02_Sintaxis_variables_palabras_reservadas/index.js
@@ -0,0 +1,14 @@
+var name = "Nuria";
+var old = 32;
+var dev = false;
+const born = new Date("1990 08 08") 
+const book = {
+    title: "La cuenta atrás para el verano",
+    autor: "La Vecina Rubia",
+    date: 2021,
+    url: "fnljsdbksnlk",
+}
+
+lista = [name, old, dev, born, book];
+
+console.log(lista);
